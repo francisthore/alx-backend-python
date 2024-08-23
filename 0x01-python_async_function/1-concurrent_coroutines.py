23e5f7d91fcc4907a7ce74a12f841c58c3f40558
Francis Thore <thorefrancis@gmail.com>
2024-08-23 22:53:28 +0200

    return List[float]

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,18 @@
 """Module that defines a routine that imports the
 wait_random coroutine and spawns it n times"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+def return_sorted_list(unsorted_list: List[float]) -> List[float]:
+    """Returns a sorted list"""
+    return sorted(unsorted_list)
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns a corouting up to n times with max_delay
     as the input and returns the list of delays"""
     input = []
     for i in range(n):
         input.append(wait_random(max_delay))
     res = await asyncio.gather(*input)
-    return (sorted(res))
+    return return_sorted_list(res)
