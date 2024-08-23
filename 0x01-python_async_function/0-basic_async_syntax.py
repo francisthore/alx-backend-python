71e1b787a57fe5476a688fc5aa919e04c1fd7bd8
Francis Thore <thorefrancis@gmail.com>
2024-08-23 21:37:08 +0200

    asleep

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module that defines a coroutine that
+    waits for a random delay between an
+    input 0 and a n input value
+"""
+
+import random
+import asyncio
+
+async def wait_random(max_delay = 10):
+    """Waits for a random delay between 0 and the
+    passed in value for max_delay
+    """
+    sleep_time = random.uniform(0, max_delay)
+    await asyncio.sleep(sleep_time)
+    return sleep_time
