425629cea4ca259f09a70e2cd64fa8ae0195722b
Francis Thore <thorefrancis@gmail.com>
2024-08-23 21:56:16 +0200

    cast into float

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ async def wait_random(max_delay=10):
     """Waits for a random delay between 0 and the
     passed in value for max_delay
     """
-    sleep_time = random.uniform(0, max_delay)
+    sleep_time: float = random.uniform(0, max_delay)
     await asyncio.sleep(sleep_time)
     return sleep_time
