4391ee14e9b7ca6b3b9f006b47adce7714d98971
Francis Thore <thorefrancis@gmail.com>
2024-10-03 00:38:11 +0200

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module that defines a floor function
+    which returns the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float"""
+    return int(n)
