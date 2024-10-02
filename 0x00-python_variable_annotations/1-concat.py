ad91688596c08b3236e2291b7f88b7c0fa0ede9e
Francis Thore <thorefrancis@gmail.com>
2024-10-03 00:30:09 +0200

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Module that defines a function which is typed
+    that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return '{}{}'.format(str1, str2)
