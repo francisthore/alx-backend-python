f8c106dccb7c52af25c18db51cdca9102a87f3a4
Francis Thore <thorefrancis@gmail.com>
2024-10-03 00:15:18 +0200

    typed function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that defines a typed function that adds 2
+int values
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns the sum of two floats
+    """
+    return a + b
